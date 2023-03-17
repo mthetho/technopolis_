61d50d460a6e51abaeacc415b38b17fd01cf0678
mthetho <100479996+mthetho@users.noreply.github.com>
2023-03-17 05:49:55 +0000

    made the webpage responsive and added a notification API

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect
+import replit
+import os
 
 app = Flask(__name__)
 
-JOBS = [
+message = "Welcome to tutor++, presented to you by Technopolis"  
+endpoint = os.environ.get('d503HtSDCDTzLguopDXt')  
+
+if endpoint:
+    command = f"curl https://notify.run/{endpoint} -d '{message}'"
+    os.system(command)
+else:
+    print("Endpoint not found. Please set the NOTIFY_ENDPOINT environment variable.")
+
+replit.db["jobs"] = [
   {
     'id': 1,
     'title': 'Tutor Position in Mathematics',
@@ -13,7 +24,7 @@ JOBS = [
   {
     'id': 2,
     'title': 'Tutor Position in Cornerstone',
-    'locatiion': 'ML Sultan Campus',
+    'location': 'ML Sultan Campus',
     'salary': 'R1500.00'
   },
   {
@@ -22,21 +33,66 @@ JOBS = [
     'location': 'Ritson Campus',
     'salary': 'R2000.00'
   }
-
 ]
 
 @app.route("/")
-def hello_jovian():
-  return render_template('home.html',
-                        jobs=JOBS, company_name = "Tutor++")
+def home():
+    jobs = replit.db["jobs"]
+    return render_template('home.html', jobs=jobs, company_name="Tutor++")
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+    jobs = replit.db["jobs"]
+    return jsonify(jobs)
 
+@app.route("/admin/edit_job", methods=["GET", "POST"])
+def edit_job():
+    jobs = replit.db["jobs"]
+    if request.method == "POST":
+        # Update existing job
+        job_id = int(request.form["id"])
+        for job in jobs:
+            if job["id"] == job_id:
+                job["title"] = request.form["title"]
+                job["location"] = request.form["location"]
+                job["salary"] = request.form["salary"]
+                break
+        # Add new job
+        else:
+            job = {
+                "id": len(jobs) + 1,
+                "title": request.form["title"],
+                "location": request.form["location"],
+                "salary": request.form["salary"]
+            }
+            jobs.append(job)
+        # Save changes to database
+        replit.db["jobs"] = jobs
+        return redirect("/")
+    else:
+        job_id = int(request.args.get("id", "0"))
+        if job_id == 0:
+            # Render form to add new job
+            return render_template("edit_job.html", job=None)
+        else:
+            # Render form to edit existing job
+            for job in jobs:
+                if job["id"] == job_id:
+                    return render_template("edit_job.html", job=job)
+            # Job not found
+            return redirect("/")
+  
+@app.route("/home/")
+def home_page():
+  return render_template('home.html')
+  
 @app.route("/login/")
 def login_page():
   return render_template('login.html')
+
+@app.route("/admin/")
+def admin_page():
+  return render_template('admin.html')
 
 @app.route("/documents/")
 def documents_page():
