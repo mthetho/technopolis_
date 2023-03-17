a1d569464ab33cebe9194758b7e82c03e34cde2a
mthetho <100479996+mthetho@users.noreply.github.com>
2023-03-17 10:02:06 +0000

    added requirements

--- app.py
+++ app.py
@@ -90,6 +90,10 @@ def home_page():
 def login_page():
   return render_template('login.html')
 
+@app.route("/dashboard/")
+def dashboad_page():
+  return render_template('dashboard.html')
+
 @app.route("/admin/")
 def admin_page():
   return render_template('admin.html')
